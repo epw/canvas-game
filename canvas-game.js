d89a29d62671f4bfc1ceca5b7c4c127082368c1b
Eric Willisson <ericwillisson@gmail.com>
2011-05-20 13:52:28 -0400

    Fixed screen clip, added period to keys

--- canvas-game.js
+++ canvas-game.js
@@ -3,7 +3,7 @@
 // Game constants
 var FRAME_RATE = 30; // Can be set by application
 var KEY = { RIGHT:39, UP:38, LEFT:37, DOWN:40, SPACE:32, ESCAPE:27, RETURN:13,
-	    SHIFT:16, CONTROL:17, ALT:18};
+	    SHIFT:16, CONTROL:17, ALT:18, PERIOD:190};
 
 // Game variable, can be altered. DECLARE IF NEEDED
 //var screen_clip = {"x": 0, "y": 0, "w": 800, "h": 600};
